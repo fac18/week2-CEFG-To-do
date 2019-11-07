be0a8b69fc3af65a33fd8cf17cb057106175dcaa
Maria <m.olanipekun@hotmail.com>
2019-11-07 14:53:31 +0000

    marktodo-dom

--- logic.js
+++ logic.js
@@ -75,8 +75,9 @@ var todoFunctions = {
 // The answer has something to do with needing to run code both in the browser and in Node.js
 // See this article for more details: 
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
-/*if (typeof module !== 'undefined') {
+
+if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
-}*/
+}
 
 module.exports = todoFunctions;
